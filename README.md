352161d149584465104d75880af55ab570d6e2c1
Cristian Camargo <chris.camargo2015@gmail.com>
2021-10-09 10:49:44 -0400

    Adicionando biblioteca scrollReveal e finalizando aula 3

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Mission Origin - NLW Togueter
 - Esse projeto foi feito no NLW Togueter, para acompanhar as aulas, e adquirir mais conhecimento na base.
 
 - A missão origin do NLW era uma missão mais voltada para um publico iniciante, onde abordamos HTML, CSS e JS basico para construção de paginas mais simples.
+
+- Utilizamos aqui algumas bibliotecas prontas tambem como Swiper para fazer o carrousel de depoimentos, e o ScrollReveal JS para criar o efeito das coisas aparecerem quando rolamos a página
+
+- essas bibliotecas se encontram no links:
+
+-- https://scrollrevealjs.org/
+-- https://swiperjs.com/
+
+
--- js/main.js
+++ js/main.js
@@ -35,6 +35,8 @@ window.addEventListener('scroll', () => {
     }
 });
 
+/* swipper */
+
 
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
@@ -45,3 +47,20 @@ const swiper = new Swiper('.swiper', {
     keyboard: true
 });
 
+
+/* Scroll reveal */
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration:700,
+    reset: true
+})
+
+scrollReveal.reveal(`
+                    #home .image, #home .text,
+                    #about .image, #about .text,
+                    #services header, #services .card,
+                    #testimonials header, #testimonials .testimonials,
+                    #contact .text, #contact .links
+                    `, {interval:100})
